1ec5243711da272180d8abe023010899f1612a34
Kimi Nikolov <bytebuilder220@gmail.com>
2023-06-18 17:56:05 +0300

    Remade book-a-car using grid

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-let scrollToTopButton = document.getElementById("goToTopButton");
-
-window.onscroll = () => {
-    scrollDown();
-};
-
-function scrollDown() {
-  if (document.documentElement.scrollTop > 20) {
-    scrollToTopButton.style.display = "block";
-  } else {
-    scrollToTopButton.style.display = "none";
-  }
-}
-
-function goToTop() {
-  document.documentElement.scrollTop = 0;
-}
-
 let logo = document.querySelector(".logo-name");
 logo.onclick = () => {
     window.location.href = "/main.html";
