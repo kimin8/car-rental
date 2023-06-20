b35e3425fe53eba93c96329b879c191d606f29a5
Kimi Nikolov <bytebuilder220@gmail.com>
2023-06-20 19:59:07 +0300

    Burger officially works

--- script.js
+++ script.js
@@ -51,13 +51,19 @@ const links = Array.from(document.querySelector('.links').children);
 links.forEach(link => {
   link.onclick = () => {
     curtain.style.display = "none";
+    document.documentElement.style.overflow = "auto";
+    document.body.style.overflow = "auto";
   }
 });
 
 hamburger.onclick = () => {
   curtain.style.display = "block";
+  document.documentElement.style.overflow = "hidden";
+  document.body.style.overflow = "hidden";
 }
 
 closebutton.onclick = () => {
   curtain.style.display = "none";
+  document.documentElement.style.overflow = "auto";
+  document.body.style.overflow = "auto";
 }
